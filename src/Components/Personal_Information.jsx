1c717e59cc771c5568c512abdf0ed149ae5ed78f
josen190 <slava-zakharov-1990@mail.ru>
2023-01-31 23:37:22 +0400

    добавлен прототип страницы акаунта

---
+++ src/Components/Personal_Information.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Personal_Information extends Component {
+  render() {
+    return (
+      <div>Personal_Information</div>
+    )
+  }
+}
--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import Personal_Information from '../Components/Personal_Information'
+import Tidings from '../Components/Tidings'
+import logo from '../logo.svg'
 
 export default class Account extends Component {
   render() {
     return (
-      <div>Account</div>
+      <main>
+        <div>
+          <div>
+            <img src={logo} alt="аватар" />
+          </div>
+          <div>
+            <div>
+              <a href='/setting'>Настройки</a>
+            </div>
+            <div>
+              <button>Подписаться</button>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Personal_Information></Personal_Information>
+          <div>
+            <Tidings></Tidings>
+          </div>
+        </div>
+
+      </main>
     )
   }
 }
