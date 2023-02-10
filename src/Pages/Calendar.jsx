36590ff8ac181de3674d4cb2fac03651e6ccdacc
josen190 <slava-zakharov-1990@mail.ru>
2023-02-10 13:19:36 +0400

    +

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -17,11 +17,11 @@ const Calendar = () => {
   console.log(date);
 
   return (
-    <>
+    <div className="scroll-fix parts-2-page">
       <Header></Header>
       <div className="mg-10-auto grid-col-2 ">
         <Menu></Menu>
-        <main className="tile scroll-fix">
+        <main className="tile calendar">
           <Month year={date.getFullYear()} numberMonth={date.getMonth()} />
           <div>
             <Button type="button" onClick={prevMonth} value="Предыдущий" />
@@ -29,7 +29,7 @@ const Calendar = () => {
           </div>
         </main>
       </div>
-    </>
+    </div>
   );
 };
 export default Calendar;
