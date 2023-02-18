dfa0b20b9799f63ca82a2858ab0026f7cc9902d6
josen190 <slava-zakharov-1990@mail.ru>
2023-02-18 23:33:24 +0400

    добавлены пути api для авторизации

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,3 +1,10 @@
+
+const host = 'https://localhost:7116'
 //авторизация
+
 //возвращяет: jwt(string), refreshToken(string)
-export const authenticate = 'https://localhost:7116/api/authentication/authenticate';
+export const authenticate = `${host}/api/authentication/authenticate`;
+export const refresh = `${host}/api/authentication/refresh`;
+export const change_password = `${host}/api/authentication/change_password`;
+export const send_reset_password_url = `${host}/api/authentication/send_reset_password_url`;
+export const reset_password = `${host}/api/authentication/reset_password`;
