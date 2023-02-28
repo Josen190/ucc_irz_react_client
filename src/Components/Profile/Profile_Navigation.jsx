7ead3fe375f11d337bc2bf27b3d10365dd3f3309
josen190 <slava-zakharov-1990@mail.ru>
2023-02-28 11:08:13 +0400

    правил

--- src/Components/Profile/Profile_Navigation.jsx
+++ src/Components/Profile/Profile_Navigation.jsx
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 import Button from '../basic/Button'
 
-export default function Profile_Navigation ({isLogn: isLogin}){
- 
-    
+export default function Profile_Navigation ({isLogin}){
     return (
         <div className='content-centr column w-200px'>
             {isLogin && <Button type='link' href='/edit' color='mini'>Редактировать профиль</Button>}
