16266683b651ed92809f71e05eccb586041c8516
josen190 <slava-zakharov-1990@mail.ru>
2023-02-28 11:03:21 +0400

    добавлено разделения на свой и чужой аккаунт

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -2,43 +2,38 @@ import React, { Component, useState, useContext } from "react";
 import Personal_Information from "../Components/Profile/Personal_Information";
 import Profile_Navigation from "../Components/Profile/Profile_Navigation";
 import Profile_Picture from "../Components/Profile/Profile_Picture";
-import Tidings from "../Components/News/Tidings";
 import Button from "../Components/basic/Button";
 import CreateTidings from "../Components/News/CreateTidings";
-import Header from "../Components/Header/Header";
-import Menu from "../Components/Menu/Menu";
 import "./pages.css";
-import User from "./User";
-import { authContext } from "../api/authentication/authController";
 import { Navigate, useLoaderData } from "react-router";
-import API, { catchApi, url_user_id } from "../api/Api";
-import axios from "axios";
+import API, { url_user_id } from "../api/Api";
 import FeedNews from "../Components/News/FeedNews";
+import { authContext } from "../api/authentication/authController";
 
-export async function accountLoader ({params}) {
+export async function accountLoader({ params }) {
+  const res = await API.get(url_user_id(params.id)).catch((error) => {});
 
-  const res = await API.get(url_user_id(params.id)).catch((error) => console.log(error));
-  console.log(res);
-
-  return res ? res.data : null ;
-  
+  return res ? res.data : null;
 }
 
 const Account = () => {
   const [active, setActive] = useState(false);
+  const { authData } = useContext(authContext);
   const result = useLoaderData();
-  console.log(result);
+  let isLogin = authData.myID === result.id ? authData.myID !== null : false;
+  console.log(isLogin);
 
   return (
-      <main className="account">
-        <div className="tile ProfileHeader">
-          <div className="margin-right">
-            <Profile_Picture type="norm"></Profile_Picture>
-            <Profile_Navigation></Profile_Navigation>
-          </div>
-          <Personal_Information userInfo={result}></Personal_Information>
+    <main className="account">
+      <div className="tile ProfileHeader">
+        <div className="margin-right">
+          <Profile_Picture type="norm"></Profile_Picture>
+          <Profile_Navigation isLogin={isLogin}></Profile_Navigation>
         </div>
-        <div className="">
+        <Personal_Information userInfo={result}></Personal_Information>
+      </div>
+      <div className="">
+        {isLogin && (
           <div className="tile">
             <Button
               type="button"
@@ -49,11 +44,11 @@ const Account = () => {
               Создать новость
             </Button>
           </div>
-          <FeedNews userID={result.id}/>
-        </div>
-        {active && <CreateTidings setActive={setActive} />}
-      </main>
-
+        )}
+        <FeedNews userID={result.id} />
+      </div>
+      {active && <CreateTidings setActive={setActive} />}
+    </main>
   );
 };
 
