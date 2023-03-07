87d62513ccafc35dd3e2406d5fbd04cc81859c89
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-07 22:37:39 +0400

    отпрправка коментариев

--- src/Components/News/CommentFeed.jsx
+++ src/Components/News/CommentFeed.jsx
@@ -1,16 +1,15 @@
-import React, {Component} from "react";
-import API, { url_get_comments } from "../../api/Api";
+import React, { Component } from "react";
+import API, { url_get_news_comments } from "../../api/Api";
 import Comment from "./Comment";
-
+import CreateComment from "./CreateComment";
 export default class CommentFeed extends Component {
   constructor(props) {
     super(props);
-
     this.state = { commentArr: [] };
   }
 
   componentDidMount() {
-    API.get(url_get_comments, { params: { newsEntryId: this.props.newsID } })
+    API.get(url_get_news_comments, { params: { newsEntryId: this.props.newsID } })
       .then((response) => {
         let commentArr = [];
         response.data.forEach((element, index) => {
@@ -22,7 +21,9 @@ export default class CommentFeed extends Component {
   }
 
   render() {
-    return <div className="column">{this.state.commentArr}</div>;
+    
+    return <div className="column">
+      <CreateComment newsID={this.props.newsID}/>
+      {this.state.commentArr}</div>;
   }
 }
-
--- src/Components/News/CreateComment.jsx
+++ src/Components/News/CreateComment.jsx
@@ -1,14 +1,36 @@
-import React, { Component } from 'react'
-import Button from '../basic/Button'
-import Textarea from '../basic/Textarea'
+import React, { useState } from "react";
+import API, { url_post_news_comments } from "../../api/Api";
+import Button from "../basic/Button";
+import InputField from "../basic/InputField";
+import { notifyError } from "../Notifications/Notifications";
 
-export default class CreateComment extends Component {
-  render() {
-    return (
-      <div className='tile colume'>
-        <Textarea />
-        <Button type='submit' >Добавть</Button>
-      </div>
-    )
-  }
+export default function CreateComment({ newsID }) {
+  const [text, setText] = useState("");
+  const [value, setValue] = useState("");
+  const newNewsComments = (event) => {
+    event.preventDefault();
+    const data = {
+      newsEntryId: newsID,
+      text: text,
+    };
+    API.post(url_post_news_comments, data)
+      .then(() => {
+        setText("");
+        setValue("");
+      })
+      .catch(() => {
+        notifyError("Ощибка, попробуйте снова");
+      });
+  };
+
+  return (
+    <form className="tile colume" onSubmit={(e) => newNewsComments(e)}>
+      <InputField
+        type="textarea"
+        value={value}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <Button type="submit">Добавть</Button>
+    </form>
+  );
 }
