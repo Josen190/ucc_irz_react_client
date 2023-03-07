f133220c70eb930bd2ef0ef2619bb5aa680039b1
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-07 18:29:49 +0400

    изменено на функцию

--- src/Components/News/Tidings.jsx
+++ src/Components/News/Tidings.jsx
@@ -6,23 +6,25 @@ import Like from "./Like";
 import CommentsIcon from "./CommentsIcon";
 import CommentFeed from "./CommentFeed";
 
-export default class Tidings extends Component {
-  render() {
-    return (
-      <div>
-        <div className="tile">
-          <Author author={this.props.author}></Author>
-          <Content title={this.props.title} content={this.props.text}></Content>
-          <div className="row">
-            <Like
-              likesCount={this.props.likesCount}
-              isLiked={this.props.isLiked}
-            ></Like>
-            <CommentsIcon commentCount={this.props.commentCount}></CommentsIcon>
-          </div>
+export default function Tidings(
+  author,
+  title,
+  text,
+  likesCount,
+  isLiked,
+  commentCount
+) {
+  return (
+    <div>
+      <div className="tile">
+        <Author author={author}></Author>
+        <Content title={title} content={text}></Content>
+        <div className="row">
+          <Like likesCount={likesCount} isLiked={isLiked}></Like>
+          <CommentsIcon commentCount={commentCount}></CommentsIcon>
         </div>
-        <CommentFeed></CommentFeed>
       </div>
-    );
-  }
+      <CommentFeed></CommentFeed>
+    </div>
+  );
 }
