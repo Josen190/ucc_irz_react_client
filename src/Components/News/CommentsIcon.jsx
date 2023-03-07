ed2e6973bccdfb283a7eb38c3c37256ba2f29e85
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-07 17:51:03 +0400

    добавлена иконка коментария

--- src/Components/News/CommentsIcon.jsx
+++ src/Components/News/CommentsIcon.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import ChatBoxOutline from '../icons/ChatBoxOutline'
 export default class CommentsIcon extends Component {
   render() {
     let numberComments = this.props.commentCount;
@@ -9,14 +9,15 @@ export default class CommentsIcon extends Component {
 
       disableBtnProps.disabled = true;
     };
-
+    
     let button = (
       <button
         {...disableBtnProps}
         className="icon row"
         onClick={switchComments}
       >
-        <p>{numberComments}</p>
+        <span>{numberComments}</span>
+        <ChatBoxOutline></ChatBoxOutline>
       </button>
     );
 
