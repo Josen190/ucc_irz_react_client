bc29cab5f57619500bcdafa29bb7fcb0d5835b93
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-11 18:35:20 +0400

    удаление коментариев

--- src/Components/News/Comment.jsx
+++ src/Components/News/Comment.jsx
@@ -1,21 +1,39 @@
-import React, { useContext } from 'react'
-import Button from '../basic/Button'
-import Content from '../basic/Content'
-import Author from './AuthorNews'
+import React, { useContext, useEffect } from "react";
+import Button from "../basic/Button";
+import Content from "../basic/Content";
+import Author from "./AuthorNews";
 import { authContext } from "../../api/authentication/authController";
+import API, { url_delete_news_comments_id } from "../../api/Api";
+import { notifyError, notifySuccess } from "../Notifications/Notifications";
 
-export default function Comment ({data}) {
-    const {authData} = useContext(authContext)
-    let isMyComment = authData.myID === data.user.id ? authData.myID !== null : false;
+export default function Comment({ data }) {
+  const { authData } = useContext(authContext);
+  let isMyComment =
+    authData.myID === data.user.id ? authData.myID !== null : false;
 
-    return (
-      <div className='tile'>
-        <div className='row'>
-          <Author author={data.user}/>
-          {isMyComment && <Button >Удалить</Button>}
-        </div>
-        <Content content={data.text}></Content>
+  const deletComment = () => {
+    useEffect(
+      API.delete(url_delete_news_comments_id(data.id))
+        .then(() => {
+          notifySuccess("Коментарий удалён");
+        })
+        .catch(() => {
+          notifyError("Ошибка, коментарий не удален");
+        })
+    );
+  };
+
+  return (
+    <div className="tile">
+      <div className="row">
+        <Author author={data.user} />
+        {isMyComment && (
+          <Button type="button" onClick={deletComment}>
+            Удалить
+          </Button>
+        )}
       </div>
-    )
-  }
-
+      <Content content={data.text}></Content>
+    </div>
+  );
+}
