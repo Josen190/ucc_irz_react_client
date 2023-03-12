3003e629a7cee9f0857bfafc2779790583ebaccb
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-12 15:36:11 +0400

    изменена сылка

--- src/Components/Profile/Profile_Navigation.jsx
+++ src/Components/Profile/Profile_Navigation.jsx
@@ -4,7 +4,7 @@ import Button from '../basic/Button'
 export default function Profile_Navigation ({isLogin}){
     return (
         <div className='content-centr column w-200px'>
-            {isLogin && <Button type='link' href='/edit' color='mini'>Редактировать профиль</Button>}
+            {isLogin && <Button type='link' href='/edit/info' color='mini'>Редактировать профиль</Button>}
             {!isLogin &&<Button type='button' color='red' >Подписаться</Button>}
         </div>
     )
