9966133c71adc3db8ef7365f313fae16df1dc015
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-12 16:58:22 +0400

    добавлен список имен месяцов

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -2,6 +2,21 @@ import React, { Component, useState } from "react";
 import Month from "../Components/Calendar/Month/Month";
 import Button from "../Components/basic/Button";
 
+const nameMonth = [
+  "Январь",
+  "Февраль",
+  "Март",
+  "Апрель",
+  "Май",
+  "Июнь",
+  "Июль",
+  "Август",
+  "Сентябрь",
+  "Октябрь",
+  "Ноябрь",
+  "Декабрь",
+];
+
 const Calendar = () => {
   const [date, setDate] = useState(new Date());
 
@@ -11,14 +26,15 @@ const Calendar = () => {
   const prevMonth = () => {
     setDate(new Date(date.setMonth(date.getMonth() - 1)));
   };
- 
+
   return (
     <main className="tile calendar">
       <div>
-        <div className="row">
+        <div className="center row">
           <Button type="button" onClick={prevMonth}>
             {"<"}
           </Button>
+          <p>{nameMonth[date.getMonth()]}</p>
           <Button type="button" onClick={nextMonth}>
             {">"}
           </Button>
