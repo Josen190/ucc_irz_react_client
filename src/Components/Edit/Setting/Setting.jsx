5162f5627309f3d592e54c443595fc4769e72a4c
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-12 15:46:36 +0400

    форма изменения пароля

--- src/Components/Edit/Setting/Setting.jsx
+++ src/Components/Edit/Setting/Setting.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import InputField from "../../basic/InputField";
+import API, {url_put_change_password} from "../../../api/Api";
+import Button from "../../basic/Button";
+import { notifyError, notifySuccess } from "../../Notifications/Notifications";
 
 function Setting() {
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
 
+  const save = (event) => {
+    event.preventDefault();
+
+    if (newPassword === rePassword) {
+      API.put(url_put_change_password, {
+        currentPassword: currentPassword,
+        newPassword: newPassword,
+      })
+        .then(() => {
+          notifySuccess("изменения сохранены");
+        })
+        .catch((error) => {
+          notifyError("изменения не сохранены");
+        });
+    }
+  };
 
   return (
-    <form>
-        
+    <form onSubmit={e => save(e)}>
+      <div>
+        <h4>Изменить пароль</h4>
+        <InputField
+          type={"password"}
+          placeholder={"Старый пароль"}
+          onChange={(e) => setCurrentPassword(e.target.value)}
+        />
+        <InputField
+          type={"password"}
+          placeholder={"Новый пароль"}
+          onChange={(e) => setNewPassword(e.target.value)}
+        />
+        <InputField
+          type={"password"}
+          placeholder={"Повторить пароль"}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button type="submit">Сохранить</Button>
+      </div>
     </form>
-  )
+  );
 }
 export default Setting;
