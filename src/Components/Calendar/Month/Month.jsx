2dffcf13fc569e161d63f153e198934702418ff9
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-12 16:29:10 +0400

    изменен показ дней

--- src/Components/Calendar/Month/Month.jsx
+++ src/Components/Calendar/Month/Month.jsx
@@ -13,9 +13,12 @@ function showMonth(year, numberMonth) {
 
   let indexDay = new Date(firstDayOfCalendar);
   let arrDayOfCalendar = [];
-  for (let i = 0; i < 5 * 7; i++) {
-    arrDayOfCalendar.push(new Date(indexDay));
-    indexDay.setDate(indexDay.getDate() + 1);
+  for (let i = 0; i < 5; i++) {
+    arrDayOfCalendar.push([]);
+    for (let j = 0; j < 7; j++) {
+      arrDayOfCalendar[i].push(new Date(indexDay));
+      indexDay.setDate(indexDay.getDate() + 1);
+    }
   }
 
   return arrDayOfCalendar;
@@ -58,131 +61,18 @@ export default function Month({ year, numberMonth }) {
             {nameDayWeekUse.map((e, i) => (
               <th key={i}>{e}</th>
             ))}
-            {/* <th>{nameDayWeekUse[0]}</th>
-        <th>{nameDayWeekUse[1]}</th>
-        <th>{nameDayWeekUse[2]}</th>
-        <th>{nameDayWeekUse[3]}</th>
-        <th>{nameDayWeekUse[4]}</th>
-        <th>{nameDayWeekUse[5]}</th>
-        <th>{nameDayWeekUse[6]}</th> */}
           </tr>
         </thead>
         <tbody className="month-row">
-          <tr className="month-column">
-            <td>
-              <Day day={arrDayOfCalendar[0]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[1]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[2]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[3]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[4]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[5]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[6]} month={numberMonth} />
-            </td>
-          </tr>
-          <tr className="month-column">
-            <td>
-              <Day day={arrDayOfCalendar[7]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[8]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[9]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[10]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[11]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[12]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[13]} month={numberMonth} />
-            </td>
-          </tr>
-          <tr className="month-column">
-            <td>
-              <Day day={arrDayOfCalendar[14]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[15]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[16]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[17]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[18]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[19]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[20]} month={numberMonth} />
-            </td>
-          </tr>
-          <tr className="month-column">
-            <td>
-              <Day day={arrDayOfCalendar[21]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[22]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[23]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[24]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[25]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[26]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[27]} month={numberMonth} />
-            </td>
-          </tr>
-          <tr className="month-column">
-            <td>
-              <Day day={arrDayOfCalendar[28]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[29]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[30]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[31]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[32]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[33]} month={numberMonth} />
-            </td>
-            <td>
-              <Day day={arrDayOfCalendar[34]} month={numberMonth} />
-            </td>
-          </tr>
+          {arrDayOfCalendar.map((week, indexWeek) => (
+            <tr key={indexWeek} className="month-column">
+              {week.map((day, indexDay) => (
+                <td key={indexDay}>
+                  <Day day={day} month={numberMonth} />
+                </td>
+              ))}
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
