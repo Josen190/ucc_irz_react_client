9ba89db972fd3c201c0f932128b373ecc7c84bee
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-16 22:21:51 +0400

    добавлена форма

--- src/Pages/Calendar.jsx
+++ src/Pages/Calendar.jsx
@@ -1,6 +1,7 @@
 import React, { Component, useState } from "react";
 import Month from "../Components/Calendar/Month/Month";
 import Button from "../Components/basic/Button";
+import FormNewEvent from "../Components/Calendar/FormNewEvent/FormNewEvent";
 
 const nameMonth = [
   "Январь",
@@ -19,6 +20,7 @@ const nameMonth = [
 
 const Calendar = () => {
   const [date, setDate] = useState(new Date());
+  const [active, setActive] = useState(new Date());
 
   const nextMonth = () => {
     setDate(new Date(date.setMonth(date.getMonth() + 1)));
@@ -27,20 +29,31 @@ const Calendar = () => {
     setDate(new Date(date.setMonth(date.getMonth() - 1)));
   };
 
+  const newEvent = () => {
+    
+  }
+
+
   return (
     <main className="tile calendar">
       <div>
-        <div className="center row">
+        <div className="row">
+          <div className="row">
           <Button type="button" onClick={prevMonth}>
             {"<"}
           </Button>
-          <p>{nameMonth[date.getMonth()]}</p>
+          <span>{nameMonth[date.getMonth()]}</span>
           <Button type="button" onClick={nextMonth}>
             {">"}
+          </Button>
+          </div>
+          <Button type="button" onClick={() => setActive(true)}>
+            Добавить событие
           </Button>
         </div>
       </div>
       <Month year={date.getFullYear()} numberMonth={date.getMonth()} />
+      {active && <FormNewEvent />}
     </main>
   );
 };
