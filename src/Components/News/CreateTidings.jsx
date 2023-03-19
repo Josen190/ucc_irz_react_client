aec7aa8ea68a45442e9859aaef22ee923706a5e2
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-03-19 15:25:28 +0400

    отправка изображений на сервер

--- src/Components/News/CreateTidings.jsx
+++ src/Components/News/CreateTidings.jsx
@@ -7,16 +7,53 @@ import InputField from "../basic/InputField";
 import { notifyError, notifySuccess } from "../Notifications/Notifications";
 import { useEffect } from "react";
 
-export default function CreateTidings({ setActive , updateNews}) {
+function mergeMaps(map1, map2) {
+  const mergedMap = new Map();
+  
+  map1.forEach((value, key) => mergedMap.set(key, value));
+  map2.forEach((value, key) => mergedMap.set(key, value));
+  
+  return mergedMap;
+}
+
+
+
+export default function CreateTidings({ setActive, updateNews }) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState(new Map());
   const [isGlobal, setIsGlobal] = useState(false);
 
   const handleImageChange = (event) => {
     const files = Array.from(event.target.files);
-    const imagesUrls = files.map((file) => URL.createObjectURL(file));
-    setImages((prevImages) => [...prevImages, ...imagesUrls]);
+    const processFiles =  new  Promise((resolve) => {
+      const _images = new Map();
+  
+      let processedCount = 0;
+      files.forEach((file) => {
+        const sendFile = {}
+        sendFile.name = file.name.replace(/\.[^/.]+$/, "")
+        sendFile.extension = file.name.split('.').pop();
+        
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          sendFile.data = reader.result.replace("data:", "").replace(/^.+,/, "");
+          _images.set(URL.createObjectURL(file), sendFile);
+          processedCount++;
+          if (processedCount === files.length) {
+            resolve(_images);
+          }
+        };
+        reader.readAsDataURL(file);
+      });
+    });
+
+
+    processFiles.then((result) => {
+      setImages((prevImages) => {
+        return mergeMaps(prevImages, result);
+      })
+    })
   };
 
   const createNews = (event) => {
@@ -26,22 +63,32 @@ export default function CreateTidings({ setActive , updateNews}) {
       title: title,
       text: content,
       isPublic: isGlobal,
+      image: { }
     };
+
+    images.forEach((file) => {
+      data.image.name = file.name;
+      data.image.extension = file.extension;
+      data.image.data = file.data;
+    })
+
+    console.log(images);
     API.post(url_post_news, data)
       .then((response) => {
         notifySuccess("Новость создана");
         setActive(false);
         setTitle("");
         setContent("");
-        setImages([]);
+        setImages(new Map());
         setTitle(false);
-        notifySuccess("Новость создана");
-        if (typeof updateNews.update === 'function') updateNews.update();
+        if (typeof updateNews.update === "function") updateNews.update();
       })
       .catch((error) => {
         notifyError("Новость не создана, попробуйте снова");
       });
   };
+
+  console.log(images);
 
   return (
     <div
@@ -68,14 +115,16 @@ export default function CreateTidings({ setActive , updateNews}) {
         />
         <label>
           Добавить картинки:
-          <input type="file" multiple onChange={handleImageChange} />
+          <input type="file" onChange={handleImageChange} />
         </label>
         <ul>
-          {images.map((imageUrl) => (
-            <li key={imageUrl}>
-              <img className="image" src={imageUrl} alt="Новость" />
-            </li>
-          ))}
+          {Array.from(images.keys()).map((imageUrl) => {
+            return (
+              <li key={imageUrl}>
+                <img className="image" src={imageUrl} alt="Новость" />
+              </li>
+            );
+          })}
         </ul>
 
         <Checkbox
