b2728c9b8128436ddd3d4e5c6dce04ee257ca3c0
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-04-29 14:46:54 +0400

    начало фильтра новостей

---
+++ src/UI/Select/Select.tsx
@@ -0,0 +1,37 @@
+import React, { SetStateAction } from 'react'
+
+type typeValue = number | string | boolean | undefined;
+
+interface Props<T extends typeValue> {
+    children: React.ReactElement<typeof Option> | React.ReactElement<typeof Option>[];
+    setValue: React.Dispatch<SetStateAction<T>>;
+}
+
+function parser<T>(value: string): T {
+    if (Number(value))
+        return Number(value) as T;
+
+    switch (value) {
+        case 'undefined':
+            return undefined as T;
+        case 'true':
+            return true as T;
+        case 'false':
+            return false as T;
+        default:
+            return value as T;
+    }
+}
+
+
+function Select<T extends typeValue>({ children: optios, setValue }: Props<T>) {
+    return (
+        <select onChange={(e) => {
+            setValue(parser<T>(e.target.value));
+        }}>
+            {optios}
+        </select>
+    )
+}
+
+export default Select
