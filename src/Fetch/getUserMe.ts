f324f2c0a46e278ea0cfa8b412c4140399b22d30
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-05-19 22:35:39 +0400

    небольшое исправление

--- src/Fetch/getUserMe.ts
+++ src/Fetch/getUserMe.ts
@@ -1,8 +1,8 @@
 import User from "Helpers/User";
 import PropsUser from "./Interface/IUser";
 import fetch from "./Fetch";
+import {url_get_users_me} from "../Constatnts/url";
 
-const url_get_users_me = `/api/users/me`;
 
 async function getUserMe(): Promise<User> {
     return await fetch.get(url_get_users_me)
