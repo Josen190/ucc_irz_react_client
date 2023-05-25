27a751ab5ecdf90cba83717c4fafe703ab716562
Josen190 <92809532+Josen190@users.noreply.github.com>
2023-05-25 16:20:43 +0400

    исправлены ошибки

--- src/Modules/AdimnsrateUser/Fetch/updateRoles.ts
+++ src/Modules/AdimnsrateUser/Fetch/updateRoles.ts
@@ -4,7 +4,7 @@ import fetch from "Fetch/Fetch";
 const url_post_roles_add_to_user = "/api/roles/add_to_user";
 const url_post_roles_remove_from_user = "/api/roles/remove_from_user";
 
-function updateRoles(id: string, oldRole: string[], newRole: string[]) {
+async function updateRoles(id: string, oldRole: string[], newRole: string[]) {
     const addRole = newRole.filter((role) => {
         return !oldRole.includes(role);
     })
@@ -13,22 +13,20 @@ function updateRoles(id: string, oldRole: string[], newRole: string[]) {
         return !newRole.includes(role);
     })
 
-    console.log(addRole);
-
-    addRole.forEach(role => {
-        console.log(role);
-        fetch.post(url_post_roles_add_to_user, {
+    const addPromise = addRole.map(role => {
+         return  fetch.post(url_post_roles_add_to_user, {
             userId: id, 
             role,
-        }).then()
+        });
     })
 
-    removeRole.forEach(role => {
-        fetch.post(url_post_roles_remove_from_user, {
+    const removePromise = removeRole.map(role => {
+        return fetch.post(url_post_roles_remove_from_user, {
             userId: id, 
             role,
-        }).then()
+        });
     })
-    
+
+    return await Promise.allSettled([...addPromise, ...removePromise])
 }
 export default updateRoles;
--- src/UI/InputImg/InputImg.tsx
+++ src/UI/InputImg/InputImg.tsx
@@ -1,30 +1,35 @@
 import Image from "Helpers/Image";
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 import "./inputImg.scss"
 import ImageOutline from "../../Assets/icons/ImageOutline";
 
 interface Props {
   setImageApi: (image: Image | null) => unknown;
   view: "avatar" | "messenger" | "news"
-  value?: Image | null;
 }
 
-export default function InputImg({ view, setImageApi, value }: Props) {
+export default function InputImg({ view, setImageApi }: Props) {
   const [image, setImage] = useState<Image | null>(null);
 
-  useEffect(() => {
-    setImage(value ?? null);
-  }, [value])
-
   const setImageAll = (file: File | null) => {
+
     if (file === null) {
       setImage(null);
       setImageApi(null);
       return;
     }
+
     const _image = Image.getImg(file);
     setImage(_image);
-    setImageApi(_image);
+
+    // Image.toBase64(file).then(image => {
+    //   setImage(image);
+    //   setImageApi(image);
+    // }).catch(() => {
+    //   setImage(null);
+    //   setImageApi(null);
+    // })
+
   }
   const className = `preview-${view}`;
   let title: string | JSX.Element;
@@ -44,16 +49,12 @@ export default function InputImg({ view, setImageApi, value }: Props) {
   return (
     <div className={"input-img" + " view-" + view}>
       <div className={className}>
-        <img src={image?.url} alt={image?.name}/>
+        <img src={image?.url} />
       </div>
       <div className={"img-"+view}>
         <label>
           {title}
-          <input
-              type="file"
-              accept="image/png, image/jpeg"
-              onChange={e => { setImageAll(e.target.files ? e.target.files.item(0) : null) }}
-          />
+          <input type="file" accept="image/png, image/jpeg" onChange={e => { setImageAll(e.target.files ? e.target.files.item(0) : null) }} />
         </label>
       </div>
     </div>
